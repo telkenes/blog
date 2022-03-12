b9e855c8306645e3a9c7fa11b69bd90f51bad06d
MarsRon <71264052+MarsRon@users.noreply.github.com>
2022-03-13 02:24:54 +0800

    Remove unnecessary template literal

--- components/blog-card.js
+++ components/blog-card.js
@@ -1,14 +1,14 @@
 import Link from 'next/link'
 export default function BlogCard({ title, description, date, time, slug }) {
     return (
-        <Link as={`/posts/${slug}`} href={`/posts/[slug]`} key={`${slug}`}>
+        <Link as={`/posts/${slug}`} href={`/posts/[slug]`} key={slug}>
             <div className="card blog-card">
                 <div className="card-header">
                     <h2 className="card-header-title">
                         {title}
                     </h2>
                     <h3 className="subtitle">
-                        - Written on: {date}<br></br>
+                        - Written on: {date}<br />
                         - Estimate read time: {time}
                     </h3>
                 </div>
